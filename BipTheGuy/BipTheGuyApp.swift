857e03f8ff8d3280c3819b71674dd4da2a38a022
John Kearon <jkearon@hotmail.com>
2025-03-30 08:49:47 -0400

    Initial Commit

---
+++ BipTheGuy/BipTheGuyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BipTheGuyApp.swift
+//  BipTheGuy
+//
+//  Created by John Kearon on 3/30/25.
+//
+
+import SwiftUI
+
+@main
+struct BipTheGuyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
